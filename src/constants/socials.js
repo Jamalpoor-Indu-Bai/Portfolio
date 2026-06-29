29f5bbed463396c9110b29a21015c03b28b86a07
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-06-29 14:34:31 +0530

    Added social urls

---
+++ src/constants/socials.js
@@ -0,0 +1,14 @@
+export const socials=[
+    {
+        name:"github",
+        url:"https://github.com/Jamalpoor-Indu-Bai"
+    },
+    {
+        name:"linkedin",
+        url:"https://www.linkedin.com/in/jamalpoor-indu-bai/"
+    },
+    {
+        name:"email",
+        url:"mailto:indujamalpur16@gmail.com"
+    },
+];
