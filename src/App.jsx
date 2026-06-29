ff43fc0dbe5981a3b3b24b4a55c318a7ca143490
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-06-29 14:14:54 +0530

    Tested whether tailwindcss is working or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div className="bg-black text-white h-screen flex justify-center items-center">
+      <h1 className="text-green-500 text-5xl">Portfolio</h1>
     </div>
   );
 }
