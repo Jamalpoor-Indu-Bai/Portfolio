8d037c4dc4f6cdc0e35e903bc1b23a705f48472b
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-06-29 14:24:06 +0530

    Added skills

---
+++ src/constants/skills.js
@@ -0,0 +1,43 @@
+export const skills=[
+    {
+        name:"Java",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"Spring Boot",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"React",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"MySQL",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"Data structures and Algorithms",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"HTML",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"tailwindcss",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        level:"Intermediate"
+    },
+    {
+        name:"github",
+        icon:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        level:"Intermediate"
+    }
+
+];
