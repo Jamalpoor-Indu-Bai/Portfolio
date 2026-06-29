b11228b3fcc9f997ac5a4d504f0f284b1e2de0f5
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-06-29 14:30:12 +0530

    Storing the projects over here

---
+++ src/constants/projects.js
@@ -0,0 +1,53 @@
+export const projects=[
+    {
+         title: "Portfolio",
+    description: "My personal portfolio website.",
+    technologies: [
+      "React",
+      "Tailwind CSS",
+      "Framer Motion",
+    ],
+    github: "",
+    liveDemo: "",
+    image: "",
+  },
+  {
+    title:"NeuroFleet-x",
+    description:"A web application for managing and tracking fleet vehicles using IoT and AI technologies.",
+    technologies:[
+        "React",
+        "Springboot",
+        "MySQL",
+        "ETA prediction",
+    ],
+    github:"",
+    liveDemo:"",
+    image:"",
+  },
+  {
+    title:"TripNova-AI Travel Planner",
+    description:"An AI-powered travel planning application that generates personalized itineraries based on user preferences and interests.",
+    technologies:[
+        "React",
+        "Springboot",
+        "MySQL",
+        "AI-powered itinerary generation",
+    ],
+    github:"",
+    liveDemo:"",
+    image:"",
+  },
+  {
+    title:"SKILL4SHINE",
+    description:"Skill Development and Interview Preparation Platform",
+    technologies:[
+        "React",
+        "Django",
+        "Flask",
+        "MySQL",
+    ],
+    github:"",
+    liveDemo:"",
+    image:"",
+  }
+]
