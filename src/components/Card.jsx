e5ef5f5f9dbbe5acb89660c6ca57b59bf6e84ef7
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-07-07 10:13:12 +0530

    changed the color of background and text and hover from emerald to green

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({children,className=""}) => {//here className allows you to pass in additional classes to the Card component. This is useful if you want to customize the styling of the Card component for a specific use case. The default value for className is an empty string, so if you don't pass in any additional classes, the Card component will still have its default styling applied.
+const Card = ({ children, className = "" }) => {
   return (
-    <div className={`rounded-2xl border border-white/10 bg-white/5 p-6 shadow-lg backdrop-blur-md transition-all duration-300 hover:-translate-y-2 cursor-pointer hover:shadow-[0_0_30px_rgba(16,185,129,0.25)] hover:border-emerald-400 ${className}`}>
-        {children}
-      
+    <div
+      // className={`rounded-2xl border border-white/10 bg-white/5 p-6 backdrop-blur-md shadow-lg transition-all duration-300 hover:-translate-y-2 hover:border-emerald-500 hover:shadow-xl hover:shadow-emerald-500/20 ${className}`}
+      className={`bg-[#161b22]
+border border-[#30363d]
+rounded-2xl
+hover:border-[#3fb950]
+hover:-translate-y-1
+transition-all p-6 backdrop-blur-md shadow-lg
+duration-300
+${className}`}
+    > 
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
