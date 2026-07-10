1789a9bcd402c537d8d9350374106ae53708cce1
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-07-10 12:52:31 +0530

    added ContactController for contact form API

--- portfoliobackend/portfoliobackend/src/main/java/com/indu/portfoliobackend/controller/ContactController.java
+++ portfoliobackend/portfoliobackend/src/main/java/com/indu/portfoliobackend/controller/ContactController.java
@@ -1,4 +1,22 @@
 package com.indu.portfoliobackend.controller;
 
+import com.indu.portfoliobackend.dto.ContactRequest;
+import com.indu.portfoliobackend.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/contact")
+@CrossOrigin(origins = "https://localhost:5173")
 public class ContactController {
+    @Autowired
+    private EmailService emailService;
+    public ResponseEntity<String> sendMessage(@RequestBody ContactRequest request){
+        emailService.sendEmail(request);
+        return ResponseEntity.ok("Message sent Successfully");
+    }
 }
