95f7e57df67e4955841d7b45d6749102795cdd5d
Jamalpoor-Indu-Bai <indujamalpur16@gmail.com>
2026-07-10 11:18:44 +0530

    removed the sections calling and added intro calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 
-import Navbar from "./components/Navbar";
-import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Skills from "./sections/Skills";
-import Projects from "./sections/Projects";
-import Contact from "./sections/Contact";
-import Footer from "./components/Footer";
 
-function App() {
+
+import { useState } from "react";
+import Intro from "./intro/Intro";
+import Portfolio from "./Portfolio";
+
+export default function App() {
+  const [finished, setFinished] = useState(false);
+
   return (
-    
-    <div className="min-h-screen bg-gray-950 text-white">
-     <Navbar />
-     <Hero />
-     <About />
-     <Skills />
-    <Projects /> 
-    <Contact />
-    <Footer />
-     </div>
+    <>
+      {!finished && (
+        <Intro onFinish={() => setFinished(true)} />
+      )}
 
+      {finished && <Portfolio />}
+    </>
   );
-};
-  export default App;
+}
